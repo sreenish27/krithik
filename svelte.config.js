802174c55f386eeabd25daf5f8dad47cd92a3f5d
sreenish27 <118478292+sreenish27@users.noreply.github.com>
2025-08-09 18:41:53 -0700

    Fix svelte.config and deploy

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,13 @@
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  kit: {
-    adapter: adapter()
-  },
-  preprocess: vitePreprocess()
+	preprocess: vitePreprocess(),
+
+	kit: {
+		adapter: adapter()
+	}
 };
+
 export default config;
